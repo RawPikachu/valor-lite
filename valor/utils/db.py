4005be145879e6af7856b2b30de495e6aa379168
RawPikachu <102355977z@gmail.com>
2024-11-18 14:33:20 -0500

    Initial structure

--- valor/utils/db.py
+++
@@ -1,8 +0,0 @@
-from aiomysql import Pool
-
-
-async def execute_query(pool: Pool, query: str) -> list[tuple]:
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute(query)
-            return await cur.fetchall()
